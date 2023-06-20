c976bd2c127c05993e449aba85108f866b05a1cc
Rolox1992 <71694552+Rolox1992@users.noreply.github.com>
2023-06-20 11:07:26 +0100

    Finished the while statement

--- main.py
+++ main.py
@@ -14,3 +14,29 @@ user_choice = ''
 while user_choice != 'q':
     computer_choice = random.choice(options)
     user_choice = input('\nChoose Rock, Paper, or Scissors or Q to quit: ')
+
+# game logic
+    if user_choice == 'rock' and computer_choice == 'rock':
+        print(f'Computer chose {computer_choice}. This round is a draw')
+    elif user_choice == 'rock' and computer_choice == 'paper':
+        print(f'Computer chose {computer_choice}. You loose this round')
+        computer_score += 1
+    elif user_choice == 'rock' and computer_choice == 'scissors':
+        print(f'Computer chose {computer_choice}. You win this round')
+        user_score += 1
+    elif user_choice == 'paper' and computer_choice == 'rock':
+        print(f'Computer chose {computer_choice}. You win this round')
+        user_score += 1
+    elif user_choice == 'paper' and computer_choice == 'paper':
+        print(f'Computer chose {computer_choice}. This round is a draw')
+    elif user_choice == 'paper' and computer_choice == 'scissors':
+        print(f'Computer chose {computer_choice}. You loose this round')
+        computer_score += 1
+    elif user_choice == 'scissors' and computer_choice == 'rock':
+        print(f'Computer chose {computer_choice}. You loose this round')
+        computer_score += 1
+    elif user_choice == 'scissors' and computer_choice == 'paper':
+        print(f'Computer chose {computer_choice}. You win this round')
+        user_score += 1
+    elif user_choice == 'scissors' and computer_choice == 'scissors':
+        print(f'Computer chose {computer_choice}. This round is a draw')
